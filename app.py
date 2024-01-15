809c7ab561860780a62caebeb5e3c181c33fae83
H4CK3R <h4ck3r@h4ck3r.h4ck3r>
2024-01-15 18:26:05 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    subjects = ['CIT-303', 'CIT-333', 'CIT-344', 'CIT-352']  # Updated with CIT-352
+    return render_template('index.html', subjects=subjects)
+
+@app.route('/notes/<subject>/<filename>')
+def get_note(subject, filename):
+    return send_from_directory(f'notes/{subject}', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
